8031002cdcff9f2aa2b94a47cf2c1978a424398f
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-09-03 16:42:34 -0500

    chore: Added shadcn library
    Signed-off-by: Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>

--- afrotech-hack-frontend/src/App.tsx
+++ afrotech-hack-frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <h1 className="text-3xl bg-blue">Open Source Wand</h1>
-  )
+    <div>
+      <h1 className="text-3xl">Open Source Wand</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
