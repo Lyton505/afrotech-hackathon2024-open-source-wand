0c9b280582e1ce4d547f5d38bc82f0067fdf7509
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-09-03 19:37:30 -0500

    chore: Added components for the form
    Signed-off-by: Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>

---
+++ afrotech-hack-frontend/src/components/usernameForm.tsx
@@ -0,0 +1,66 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+})
+
+export function ProfileForm() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-md mx-auto">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem className="my-4">
+                            <FormLabel>Enter a Github Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Github Username" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is the Github handle name for the user you want to search for.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
+
+export default ProfileForm
