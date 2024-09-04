054c5400d05f8f9dd26f2d7c4343673bbdcc2f75
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-09-03 20:50:33 -0500

    fix: Added magic wand to hero

--- afrotech-hack-frontend/src/App.tsx
+++ afrotech-hack-frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import ProfileForm from './components/usernameForm';
+import wand from './assets/magic-wand.svg';
 
 function App() {
 
 
   return (
     <div>
-      <h1 className="text-3xl ">Open Source Wand</h1>
+      <h1 className="text-3xl mx-auto">Open Source Wand
+        <img src={wand} alt="wand" className="inline-block h-8 w-8" />
+      </h1>
       <p className="text-md my-8 mx-4 px-4">
         Open Source Wand allows you to evaluate a user's contributions to open source projects.
       </p>
