1d122929810b69963233d5097754305bd0f672f6
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-09-15 13:39:07 -0500

    added submitting animation with disabled

--- afrotech-hack-frontend/src/components/ui/loading_button.tsx
+++ afrotech-hack-frontend/src/components/ui/loading_button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const loadingButtonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950  focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
   {
     variants: {
       variant: {
@@ -49,7 +49,7 @@ const LoadingButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       >
         { children }
-        <div className="loader"></div>
+        <div className="loader mx-2"></div>
       </Comp>
     )
   }
--- afrotech-hack-frontend/src/screens/Loading.tsx
+++ afrotech-hack-frontend/src/screens/Loading.tsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from 'react';
+
 export default function Loading() {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+            window.location.href = "/results";
+        }, 2000);
+    }, []);
+
+
     return (
         <div>
             <h1>Getting your results...</h1>
