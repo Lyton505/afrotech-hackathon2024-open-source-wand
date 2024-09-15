88059a29141ae9f216c738e586a7a66cd1b7f5ed
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-09-15 17:37:22 -0500

    feat: Added results screen info
    Results screen now shows summary, categories, skill overview, and the
    evaluation table. It uses mock data. Everything is indented perfectly

--- afrotech-hack-frontend/tailwind.config.js
+++ afrotech-hack-frontend/tailwind.config.js
@@ -50,21 +50,40 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
-  	},container: {
-		center: true,
-		padding: "2rem",
-		screens: {
-		  "2xl": "1400px",
-		},
-	  },
-	  keyframes: {
-		// Add necessary keyframes here
-	  },
-	  animation: {
-		// Add necessary animations here
-	  },
-	},
+  	},
+  	container: {
+  		center: 'true',
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	keyframes: {},
+  	animation: {}
+  },
   plugins: [require("tailwindcss-animate")],
 }
 
