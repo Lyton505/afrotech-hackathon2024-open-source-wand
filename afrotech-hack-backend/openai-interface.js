7c0d60dcc01d059b48f67467e345d5813e6b934a
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-09-19 13:09:26 -0500

    gave meaningful method name and added test block

--- afrotech-hack-backend/openai-interface.js
+++ afrotech-hack-backend/openai-interface.js
@@ -8,12 +8,14 @@ const openai = new OpenAI({
 });
 
 
-const getCommitScore = async (commitMessage) => {
+const getOpenAICommitScore = async (commitMessage) => {
     const completion = await openai.chat.completions.create({
         messages: [
             { 
                 role: "system",
-                content: "You are an expert in analyzing github commit messages and determining if they are written to standard with meaning commit messages, sufficient in detail, adequate titles less than 20 characters, and contain useful information not just filler like 'fixed' or 'updated'. For each commit message you analyze, you must return a one word response of a score between 0 and 100. 100 means it is a perfect commit message, 0 means it is a bad commit message." 
+                content: `You are an expert in analyzing github commit messages and determining if they are written to standard with meaning commit messages, sufficient in detail, adequate titles less than 20 characters, and contain useful information not just filler like 'fixed' or 'updated'. 
+                You must return a one word response of a score between 0 and 100. 100 means it is a perfect commit message, 0 means it is a bad commit message.
+                ` 
             },
             { 
                 role: "user",
@@ -26,9 +28,24 @@ const getCommitScore = async (commitMessage) => {
 
     const score = completion.choices[0].message.content;
 
-    console.log("The commit score for '", commitMessage, "' is: ", score);
+    console.log(`commit: ${commitMessage} score: ${score}`);
 
     return score;
 }
 
-export default getOpenAIScore;
+const testCommitScore = async () => {
+    const testCommitMessages = [
+        "Fixes a bug in the login page that was causing the user to be logged out.",
+        "Updated the login page to fix a bug.",
+        "This is a test commit message.",
+        "Fix: Changed node version to 18.16.0 to fix a bug in custom hooks.",
+    ];
+
+    for (const commitMessage of testCommitMessages) {
+        const score = await getOpenAICommitScore(commitMessage);
+    }
+}
+
+testCommitScore();
+
+export default getOpenAICommitScore;
