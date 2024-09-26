a794793ee55e7c1e2cb951a0444db314bbe0c5d3
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-09-26 18:23:29 -0500

    fix: added key 2

--- afrotech-hack-backend/openai-interface.js
+++ afrotech-hack-backend/openai-interface.js
@@ -4,43 +4,43 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY2,
 });
 
-
 const getOpenAICommitScore = async (commitMessage, comparisonPrompt) => {
-    const completion = await openai.chat.completions.create({
-        messages: [
-            { 
-                role: "system",
-                content: comparisonPrompt 
-            },
-            { 
-                role: "user",
-                content: `The commit message is ${commitMessage}` 
-            },
-        ],
-        model: process.env.OPENAI_API_MODEL,
-        response_format: { type: "text" },
-    });
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: comparisonPrompt,
+      },
+      {
+        role: "user",
+        content: `The commit message is ${commitMessage}`,
+      },
+    ],
+    model: process.env.OPENAI_API_MODEL,
+    response_format: { type: "text" },
+  });
 
-    const score = completion.choices[0].message.content;
+  const score = completion.choices[0].message.content;
 
-    return score;
-}
+  return score;
+};
 
 const testCommitScore = async () => {
-    const testCommitMessages = [
-        "Fixes a bug in the login page that was causing the user to be logged out.",
-        "Updated the login page to fix a bug.",
-        "This is a test commit message.",
-        "Fix: Changed node version to 18.16.0 to fix a bug in custom hooks.",
-    ];
+  const testCommitMessages = [
+    "Fixes a bug in the login page that was causing the user to be logged out.",
+    "Updated the login page to fix a bug.",
+    "This is a test commit message.",
+    "Fix: Changed node version to 18.16.0 to fix a bug in custom hooks.",
+  ];
 
-    for (const commitMessage of testCommitMessages) {
-        const score = await getOpenAICommitScore(commitMessage);
-    }
-}
+  for (const commitMessage of testCommitMessages) {
+    const score = await getOpenAICommitScore(commitMessage);
+  }
+};
 
+testCommitScore();
 
 export default getOpenAICommitScore;
